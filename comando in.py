ec3f636ef46017688e21384430d6251184499669
miguel <miguel.lr2007@aluno.ifsc.edu.br>
2024-10-14 13:06:09 -0300

    aaaaa

---
+++ comando in.py
@@ -0,0 +1,9 @@
+#coding: utf=8
+vC = [1 , 3.4 , "A" , " IFSC " ]
+for i in vC :
+    print ( type(i) )
+
+def Comando_Enumerate():
+    moedas = ["BRL", "USD", "EUR"]
+for i, moeda in enumerate(moedas):
+print(i, moeda)
